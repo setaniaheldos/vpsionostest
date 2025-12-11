c875906255cd5a650db6b56c07f99728b1a42cb6
setaniaheldos <setaniaheldasseva@gmail.com>
2025-12-11 19:58:52 +0300

    Fix: écoute sur 0.0.0.0 pour le backend

--- backend/server.js
+++ backend/server.js
@@ -680,6 +680,6 @@ app.put('/rendezvous/:id', async (req, res) => {
 
 // ...existing code...
 // Démarrer serveur
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`🚀 Serveur lancé sur http://82.165.15.45:${PORT}`);
 });
